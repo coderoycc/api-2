c6bc00f92c657d8f29b1e5a6ac8a0d16edcd2dd1
BETTO <rcchambi4@umsa.bo>
2023-03-08 11:03:12 -0400

    Ciudadanos terminado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import { pool } from './db/db.js'
-dotenv.config()
+import express from "express";
+import * as dotenv from "dotenv";
+import { pool } from "./db/db.js";
+dotenv.config();
 
-const app = express()
+import DigitalRoutes from "./routes/ciudadanos.routes.js";
+const app = express();
 
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 /**
  * Probamos la conexión a la base de datos
  */
-app.get('/ping', async (req, res) => {
-  const [result] = await pool.query('SELECT "PONG" as result;')
-  res.json(result)
-})
+app.get("/ping", async (req, res) => {
+  const [result] = await pool.query('SELECT "PONG" as result;');
+  res.json(result);
+});
 
-app.listen(process.env.PORT)
-console.log('Server on port', process.env.PORT)
+app.use(DigitalRoutes);
+
+app.listen(process.env.PORT);
+console.log("Server on port", process.env.PORT);
--- src/routes/ciudadanos.routes.js
+++ src/routes/ciudadanos.routes.js
@@ -1,26 +1,37 @@
-import {Router} from 'express'
+import { Router } from "express";
 
-const router = Router()
+const router = Router();
 
-router.get('/digital/:ci',(req,res)=>{ // Mostrar uno
+router.get("/digital/:ci", (req, res) => {
+  // Mostrar uno
+  const { ci } = req.params;
+  console.log(ci);
+  res.send("Recibido");
+});
 
-})
+router.get("/digital", (req, res) => {
+  // mostrar todos
+});
 
-router.get('/digital', (req,res)=>{ // mostrar todos
+router.post("/digital", (req, res) => {
+  // recibimos en el body {ci, nombre, apellido, fecha_nac, est_civil}
+  const data = req.body;
+  console.log(data.ci);
+  console.log(data.nombre);
 
-})
+  res.send("Recibido");
+});
 
-router.post('/digital/:ci', (req,res)=>{ // Agregar
+router.put("/digital/:ci", (req, res) => {
+  // Editar
+  const { ci } = req.params;
+  res.send(`Se actualizó al usuario con CI ${ci}`);
+});
 
-})
+router.delete("/digital/:ci", (req, res) => {
+  const { ci } = req.params;
+  res.send(`Se eliminó al usuario con CI ${ci}`);
+});
 
-router.put('/digital/:ci', (req,res)=>{ // Editar
-
-})
-
-router.delete('/digital/:ci', (req,res)=>{
-
-})
-
-export default router 
+export default router;
 // DEFAULT Permite que se pueda nombrar de cualquier manera en la importación
