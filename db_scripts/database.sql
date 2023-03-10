ee738188b2c8005a46edf82e508110b984ee3779
BETTO <rcchambi4@umsa.bo>
2023-03-09 23:11:44 -0400

    Script Base de datos

--- db_scripts/database.sql
+++ db_scripts/database.sql
@@ -7,13 +7,13 @@ create table user(
   name varchar(90),
   birth_date date,
   primary key (ci)
-) charset=utf8;
+) charset=UTF8MB4;
 
 create table service(
   id_serv int(3) not null AUTO_INCREMENT, 
   descrip varchar(60) not null,
-  primary key(id_dia)
-) charset=utf8;
+  primary key(id_serv)
+) charset=UTF8MB4;
 
 
 create table queue(
@@ -22,12 +22,22 @@ create table queue(
   date_q date not null,
   id_serv int(3) not null,
   primary key(id_q),
-  foreign key id_serv references service(id_serv)
-) charset=utf8;
+  foreign key (id_serv) references service(id_serv)
+) charset=UTF8MB4;
 
 create table token(
   ci int(10) not null,
   id_q int(3) not null,
   nro int not null,
   primary key(ci, id_q) references user(ci) queue(id_q); 
-) charset=utf8;
+) charset=UTF8MB4;
+
+-- Creación del TRIGGER que actualiza la cantidad de la cola
+CREATE TRIGGER updateNumQueue
+AFTER INSERT ON token
+FOR EACH ROW 
+BEGIN
+	UPDATE queue 
+	SET quantity = NEW.nro
+	WHERE id_q = NEW.id_q; 
+END;
